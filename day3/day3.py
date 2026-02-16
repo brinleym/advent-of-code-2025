2f59c060b81887606cd754be54a29f5805136d92
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-02-16 02:23:31 -0800

    Solution to day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -26,8 +26,34 @@ class Bank:
 
         return (max_tens_digit * 10) + max_ones_digit
     
+    def max_voltage_part2(self):
+        if len(self.bank) < 12:
+            raise Exception("Invalid input")
+        
+        first_valid_index = 0
+        max_digits = [self.bank[i] for i in range(0, 12)]
+        for d in range(0, 12): 
+            # maximize value of dth digit
+            initial_first_valid_index = first_valid_index
+            last_valid_index = len(self.bank) - (12 - d)
+            
+            for i in range(first_valid_index, last_valid_index + 1):
+                if self.bank[i] > max_digits[d]:
+                    # update max_digits[d] and all subsequent
+                    # digits from index d -> 11 (inclusive)
+                    for k in range(0, 12 - d):
+                        max_digits[d + k] = self.bank[i + k]
+                    
+                    first_valid_index = i + 1
+            
+            if first_valid_index == initial_first_valid_index:
+                first_valid_index += 1
 
-def read_input(FILENAME):
+        return int("".join([str(d) for d in max_digits]))
+
+def main():
+    # Read in data
+    FILENAME = "data.txt"
     banks = []
     with open(FILENAME, 'r') as file:
         for line in file:
@@ -35,16 +61,17 @@ def read_input(FILENAME):
             bank = Bank(line)
             banks.append(bank)
 
-    return banks
-
-def main():
-    FILENAME = "data.txt"
-    banks = read_input(FILENAME)
+    # Part 1
     max_voltage_sum = 0
     for bank in banks:
         max_voltage_sum += bank.max_voltage()
-
     print("Part 1 Answer: %d" % max_voltage_sum)
+
+    # Part 2
+    max_voltage_sum = 0
+    for bank in banks:
+        max_voltage_sum += bank.max_voltage_part2()
+    print("Part 2 Answer: %d" % max_voltage_sum)
 
 main()
 
