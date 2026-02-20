72838b0ff15c62477b08a54fc717528213e9d60b
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-02-19 23:40:49 -0800

    create utils package for reusable helper functions

---
+++ utils/printing.py
@@ -0,0 +1,6 @@
+def print_solution(solution, part = 1):
+    prefix = "Solution to Part"
+    if part == 1:
+        print(f"{prefix} 1: {solution}")
+    else:
+        print(f"{prefix} 2: {solution}")
